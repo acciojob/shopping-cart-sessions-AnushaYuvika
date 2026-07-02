4163c06978c26fd73d6b8d3002888184f2cb0493
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2026-07-02 15:42:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,81 @@
+// const productList = document.querySelector("#product-list");
+// const cartList = document.querySelector("#cart-list");
+// const clearBtn = document.querySelector("#clear-cart-btn");
+
+// const products = [
+// 	{id: 1, name: "product 1", price: 10},
+// 	{id: 2, name: "product 2", price: 20},
+// 	{id: 3, name: "product 3", price: 30},
+// 	{id: 4, name: "product 4", price: 40},
+// 	{id: 5, name: "product 5", price: 50},
+// ]
+
+// function renderProducts() {
+// 	products.forEach((product) => {
+// 		const li = document.createElement("li");
+// 		li.innerHTML = `${product.name} - $${product.price}
+// 		<button>Add to cart</button>`;
+
+// 		const btn = li.querySelector("button");
+// 		btn.addEventListener("click", () => {
+// 			addToCart(product.id);
+// 		});
+// 		productList.appendChild(li);
+// 	});
+// }
+
+// function renderCart() {
+// 	cartList.innerHTML = "";
+// 	const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+// 	cart.forEach((product) => {
+// 		const li = document.createElement("li");
+// 		li.innerHTML = `${product.name} - $${product.price}`;
+// 		cartList.appendChild(li);
+// 	});
+// }
+
+// function addToCart(productId) {
+// 	const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+	// 	const product = products.find((item) => item.id === productId);
+// 	cart.push(product);
+// 	sessionStorage.setItem("cart", JSON.stringify(cart));
+// 	renderCart();
+// }
+
+// function clearCart() {
+// 	sessionStorage.removeItem("cart");
+// 	renderCart();
+// }
+
+// clearBtn.addEventListener("click", clearCart);
+
+// renderProducts();
+
+
 const productList = document.querySelector("#product-list");
 const cartList = document.querySelector("#cart-list");
 const clearBtn = document.querySelector("#clear-cart-btn");
 
 const products = [
-	{id: 1, name: "product 1", price: 10},
-	{id: 2, name: "product 2", price: 20},
-	{id: 3, name: "product 3", price: 30},
-	{id: 4, name: "product 4", price: 40},
-	{id: 5, name: "product 5", price: 50},
+	{id:1, name: "Product 1", price: 10},
+	{id:2, name: "Product 2", price: 20},
+	{id:3, name: "Product 3", price: 30},
+	{id:4, name: "Product 4", price: 40},
+	{id:5, name: "Product 5", price: 50}
 ]
 
 function renderProducts() {
-	products.forEach((product) => {
-		const li = document.createElement("li");
-		li.innerHTML = `${product.name} - $${product.price}
-		<button>Add to cart</button>`;
+	 products.forEach((product) => {
+		 const li = document.createElement("li");
+		 li.innerHTML = `${product.name} - $${product.price}
+		 <button>Add to Cart</button>`;
 
-		const btn = li.querySelector("button");
-		btn.addEventListener("click", () => {
-			addToCart(product.id);
-		});
-		productList.appendChild(li);
-	});
+		 const btn = li.querySelector("button");
+		 btn.addEventListener("click", () => {
+			 addToCart(product.id);
+		 });
+		 productList.appendChild(li);
+	 })
 }
 
 function renderCart() {
@@ -48,5 +102,6 @@ function clearCart() {
 }
 
 clearBtn.addEventListener("click", clearCart);
-
 renderProducts();
+
+	 
